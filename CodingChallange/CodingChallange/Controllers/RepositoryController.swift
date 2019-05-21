3fb495d5daddd56960a35872969ec1b775fbb843
Mounir Bittar <mounir.bittar@ideatolife.me>
2019-05-21 16:30:25 +0300

    Moved FetchData Method from Controller To ViewModel

--- CodingChallange/CodingChallange/Controllers/RepositoryController.swift
+++ CodingChallange/CodingChallange/Controllers/RepositoryController.swift
@@ -12,12 +12,8 @@ import SwiftDate
 class RepositoryController: UITableViewController, Stroyboarded {
     
     weak var coordinator: MainCoordinator?
-    private var repositoryViewModels = [RepositoryViewModel]()
+    private var repositoryViewModel = RepositoryViewModel()
     private let cellId = "RepositoryCell"
-    /// isAllLoaed used to identify if all repositories are loaded
-    private var isAllLoaded = false
-    /// current_page indicates last page loaded
-    private var current_page = 0
     
     // MARK: - Life Cycle
     
@@ -30,23 +26,15 @@ class RepositoryController: UITableViewController, Stroyboarded {
     // MARK: - private methods
     
     /// fetchData() is used to fetch stared repositories from github and display results on tableView
-    @objc fileprivate func fetchData() {
-        current_page += 1
-        /// (Date() - 30.days) gets the date 30 days ago from today
-        apiManager.fetchRepositories(date: (Date() - 30.days).toFormat("yyyy-MM-dd"), page: current_page, completion: { (response, error) in
+    fileprivate func fetchData() {
+        repositoryViewModel.fetchData { (error) in
             self.tableView.refreshControl?.endRefreshing()
             if let error = error {
                 print("Failed to fetch Repositories:", error)
                 return
             }
-            if self.current_page == 1 {
-                self.repositoryViewModels = []
-            }
-            /// if the results are incomplete then there more data to load
-            self.isAllLoaded = response!.incomplete_results
-            self.repositoryViewModels.append(contentsOf: response?.items.map({return RepositoryViewModel(repository: $0)}) ?? [])
             self.tableView.reloadData()
-        })
+        }
     }
     
     ///This function used to setup tableView and refresh control
@@ -57,34 +45,35 @@ class RepositoryController: UITableViewController, Stroyboarded {
         tableView.tableFooterView = UIView()
         tableView.refreshControl?.addTarget(self, action: #selector(refresh(sender:)), for: .valueChanged)
     }
+    
     ///This function used to reload tableView and reloads repositories from first page
     @objc fileprivate func refresh(sender:AnyObject) {
-        current_page = 0
+        repositoryViewModel.current_page = 0
         fetchData()
     }
     
     // MARK: - UITableView delegate and dataSource
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return isAllLoaded ? repositoryViewModels.count : repositoryViewModels.count + 1
+        return repositoryViewModel.isAllLoaded ? repositoryViewModel.repositoryViewModels.count : repositoryViewModel.repositoryViewModels.count + 1
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath) as! RepositoryCell
         ///if cell is loading show loading indicator else display repository content
-        cell.configure(with: isLoadingCell(for: indexPath) ? .none : repositoryViewModels[indexPath.row])
+        cell.configure(with: isLoadingCell(for: indexPath) ? .none : repositoryViewModel.repositoryViewModels[indexPath.row])
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        if indexPath.row < repositoryViewModels.count{
-            let repository = repositoryViewModels[indexPath.row]
+        if indexPath.row < repositoryViewModel.repositoryViewModels.count{
+            let repository = repositoryViewModel.repositoryViewModels[indexPath.row]
             coordinator?.openRepositoryDetails(repository)
         }
     }
     
     override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-        if indexPath.row == repositoryViewModels.count - 1 && !isAllLoaded {
+        if indexPath.row == repositoryViewModel.repositoryViewModels.count - 1 && !repositoryViewModel.isAllLoaded {
             self.fetchData()
         }
     }
@@ -92,6 +81,6 @@ class RepositoryController: UITableViewController, Stroyboarded {
 
 private extension RepositoryController {
     func isLoadingCell(for indexPath: IndexPath) -> Bool {
-        return indexPath.row >= repositoryViewModels.count
+        return indexPath.row >= repositoryViewModel.repositoryViewModels.count
     }
 }
--- CodingChallange/CodingChallange/ViewModels/RepositoryViewModel.swift
+++ CodingChallange/CodingChallange/ViewModels/RepositoryViewModel.swift
@@ -7,20 +7,49 @@
 //
 
 import UIKit
+import SwiftDate
 
-struct RepositoryViewModel {
+class RepositoryViewModel {
     
-    let name: String
-    let description: String
-    let stars_nb: String
-    let ownerUsername: String
-    let ownerAvatar: String
-
-    init(repository: Repository) {
-        self.name = repository.name ?? ""
-        self.description = repository.description ?? ""
-        self.stars_nb = "\(repository.stargazers_count)"
-        self.ownerUsername = repository.owner.login ?? ""
-        self.ownerAvatar = repository.owner.avatar_url ?? ""
-    }    
+    var name: String = ""
+    var description: String = ""
+    var stars_nb: String = ""
+    var ownerUsername: String = ""
+    var ownerAvatar: String = ""
+    
+    var repositoryViewModels: [RepositoryViewModel] = []
+    /// isAllLoaed used to identify if all repositories are loaded
+    var isAllLoaded = false
+    /// current_page indicates last page loaded
+    var current_page = 0
+    
+    init(repository: Repository? = nil) {
+        if let repository = repository {
+            self.name = repository.name ?? ""
+            self.description = repository.description ?? ""
+            self.stars_nb = "\(repository.stargazers_count)"
+            self.ownerUsername = repository.owner.login ?? ""
+            self.ownerAvatar = repository.owner.avatar_url ?? ""
+        }
+    }
+    
+    func fetchData(completion: @escaping (Error?) -> ()) {
+        current_page += 1
+        /// gets the date 30 days ago from today
+        let thirtyDaysFromToday = (Date() - 30.days).toFormat("yyyy-MM-dd")
+        apiManager.fetchRepositories(date: thirtyDaysFromToday, page: current_page, completion: { (response, error) in
+            if let error = error {
+                print("Failed to fetch Repositories:", error)
+                completion(error)
+                return
+            }
+            if self.current_page == 1 {
+                self.repositoryViewModels = []
+            }
+            /// if the results are incomplete then there more data to load
+            self.isAllLoaded = response!.incomplete_results
+            self.repositoryViewModels.append(contentsOf: response?.items.map({return RepositoryViewModel(repository: $0)}) ?? [])
+            completion(nil)
+        })
+    }
 }
